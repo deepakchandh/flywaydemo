ac117642cba5b49fca8d2a3baf1b237a37f1f39a
Deepak Chandh <deepak.chandh@tesco.com>
2025-07-22 19:41:56 +0530

    first commit

---
+++ src/main/resources/templates/V2__create_student_table.sql
@@ -0,0 +1,7 @@
+CREATE TABLE student (
+    id SERIAL PRIMARY KEY,
+    name VARCHAR(100) NOT NULL,
+    email VARCHAR(100) UNIQUE NOT NULL,
+    age INTEGER,
+    enrolled_on DATE DEFAULT CURRENT_DATE
+);
